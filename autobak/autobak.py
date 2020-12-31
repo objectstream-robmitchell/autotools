3f8abafa0e234b3827705869f523693434f67698
Rob Mitchell <rob.mitchell@objectstream.com>
2020-12-31 01:45:31 +0000

    autobak.py

--- autobak/autobak.py
+++ autobak/autobak.py
@@ -1,5 +1,10 @@
 from oslib_all_imports import *
+from config import Config
 
 class AutoBak:
-	pass
+	def __init__(self):
+		self.config = Config()()
+		print(self.config,type(self.config))
 
+
+AutoBak()
